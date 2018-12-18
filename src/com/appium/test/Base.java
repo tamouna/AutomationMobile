b3825212d9466169695150436961f9d07107e0df
Mouna TAMOUDI <mouna.tamoudi@gmail.com>
2018-12-18 10:10:44 +0100

    add dependencies

--- src/com/appium/test/Base.java
+++ src/com/appium/test/Base.java
@@ -2,33 +2,24 @@ package com.appium.test;
 
 import java.io.File;
 import java.net.MalformedURLException;
-import java.net.URL;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 
-
 public class Base {
+	
+	public static void main(String[] args) throws MalformedURLException{
 
-	public static void main(String[] args) throws MalformedURLException {
-		
-		File f = new File("src");
-		File fs = new File (f, "ApiDemos-debug.apk");
-		
-		DesiredCapabilities capabilities = new DesiredCapabilities();
-        
-		//Information about the mobile phone and the app
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        capabilities.setCapability(MobileCapabilityType.VERSION, "7.1.1");
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Galaxy A8");
-        capabilities.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
-        
-        //Connection to the server
-        
-        AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-		
-        
+		// TODO Auto-generated method stub
+	 File f = new File("src");
+     File fs = new File(f, "ApiDemos-debug.apk");
+     DesiredCapabilities cap = new DesiredCapabilities();
+     
+     cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Demo");
+     cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
+     //AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+	    
+	  
 	}
+	
 
 }
