fd5bbe08b8dd90be384bd1b25a02dabbdf5ea950
Mouna TAMOUDI <mouna.tamoudi@gmail.com>
2018-12-18 12:29:26 +0100

    errors fixed

--- src/com/appium/test/Base.java
+++ src/com/appium/test/Base.java
@@ -2,7 +2,12 @@ package com.appium.test;
 
 import java.io.File;
 import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 
 public class Base {
@@ -12,11 +17,11 @@ public class Base {
 		// TODO Auto-generated method stub
 	 File f = new File("src");
      File fs = new File(f, "ApiDemos-debug.apk");
-     DesiredCapabilities cap = new DesiredCapabilities();
      
+     DesiredCapabilities cap = new DesiredCapabilities();
      cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Demo");
      cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
-     //AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+     AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
 	    
 	  
 	}
